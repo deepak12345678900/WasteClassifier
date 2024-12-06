36eb454ce8725a92e4ed423f4c64f94cf361d68d
deepak <67847176+deepak12345678900@users.noreply.github.com>
2024-12-07 05:12:43 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask, request, render_template
 from PIL import Image
 import io
-import inference  # Assuming your custom model inference module
+import inference  
 
 app = Flask(__name__)
 
-# Initialize the model
 model = inference.get_model("waste_classifer/1", api_key="ba3svItNcD46dMWVcJKJ")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # Initialize variables for predictions
     prediction = None
     glass_conf = None
     battery_conf = None
@@ -20,14 +18,10 @@ def index():
     plastic_conf = None
 
     if request.method == 'POST':
-        # Check if the clear output button was clicked
         if 'clear_output' in request.form:
-            # Reset all prediction-related variables
             return render_template('index.html')
 
-        # Check if a file was uploaded
         if 'file' not in request.files or request.files['file'].filename == '':
-            # No need to show any error; just return the empty template
             return render_template('index.html')
 
         uploaded_image = request.files['file']
@@ -35,7 +29,6 @@ def index():
             img = Image.open(io.BytesIO(uploaded_image.read()))
             pred = model.infer(image=img)
             
-            # Extract prediction results
             prediction = pred[0].predicted_classes[0].upper()
             glass_conf = round(pred[0].predictions['Glass'].confidence, 3)
             battery_conf = round(pred[0].predictions['battery'].confidence, 3)
